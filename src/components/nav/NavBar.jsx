43509eef5fee7f504535508b172be738cb58281e
matt novotny <mattrnovotny@gmail.com>
2024-04-08 11:21:43 -0500

    auth and nav

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -3,19 +3,16 @@ import { Navbar, Nav } from 'react-bootstrap'
 import { Link, useNavigate } from 'react-router-dom'
 import './NavBar.css'
 
-const NavBar = ({ currentUser, shoppingCart }) => {
+const NavBar = ({ currentUser}) => {
   const navigate = useNavigate()
 
-  const totalItems = shoppingCart.length
 
-  const playClickSound = () => {
-    const audio = new Audio(clickSound)
-    audio.play()
-  }
+
+ 
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand as={Link} to="/" className="nav-header" onClick={playClickSound}>
+      <Navbar.Brand as={Link} to="/" className="nav-header">
         🍕 <span className="navbar-title">Shepherd's Pies</span> 🍕
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -26,7 +23,7 @@ const NavBar = ({ currentUser, shoppingCart }) => {
           <Nav.Link as={Link} to="/employees" className="nav-link">Employees</Nav.Link>
           <Nav.Link as={Link} to="/salesReport" className="nav-link">Sales Report</Nav.Link>
           <Nav.Link as={Link} to="/" onClick={() => {
-            localStorage.removeItem("currentUser")
+            localStorage.removeItem("pizza_user")
             navigate("/", { replace: true })
           }} className="nav-link">Logout</Nav.Link>
         </Nav>
--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,16 +1,15 @@
 import { Outlet, Route, Routes } from "react-router-dom"
-import { NavBar } from "../components/nav/NavBar"
-import { NurseryList } from "../components/nurseries/NurseryList"
-import { DistributorList } from "../components/distributors/DistributorList"
-import { RetailerList } from "../components/retailers/RetailerList"
+
+
 import { useEffect, useState } from "react"
-import { ShoppingCart } from "../components/users/ShoppingCart"
-import { getShoppingCartByUserId } from "../services/userService"
-import { Welcome } from "../components/welcome stream/Welcome"
+
+
+import NavBar from "../components/nav/NavBar"
+import { OrderList } from "../components/orders/OrderList"
 
 export const ApplicationViews = () => {
   const [currentUser, setCurrentUser] = useState({})
-  const [shoppingCart, setShoppingCart] = useState([])
+
   
   useEffect(() => {
     const localPizzaUser = localStorage.getItem('pizza_user')
@@ -19,24 +18,17 @@ export const ApplicationViews = () => {
     setCurrentUser(pizzaUserObject)
   }, [])
 
-  useEffect(() => {
-    if (currentUser) {
-      // Fetch shopping cart items for the current user
-      getShoppingCartByUserId(currentUser.id).then((cartArray) => {
-        setShoppingCart(cartArray);
-      });
-    }
-  }, [currentUser])
+
     return (
 
         <Routes>
           <Route path="/" element={
             <>
-            <NavBar currentUser={currentUser} shoppingCart={shoppingCart}/>
+            <NavBar currentUser={currentUser}/>
             <Outlet />
             </>
           }>
-            <Route index element={<Welcome />} />
+            <Route index element={<>yuh</> } />
             <Route path='/OrderList' element={<OrderList currentUser={currentUser} />}/>
             <Route path='/NewOrder' element={<>new order shit here bruh</> }/>
             <Route path='/Employees' element={<>employye list here bruh</> }/>
