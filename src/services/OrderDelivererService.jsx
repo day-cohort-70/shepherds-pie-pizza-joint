d93eb544a6ef72182b807e4cfa6694325770473b
Nicholas Davison <nick.a.davison@gmail.com>
2024-04-10 15:55:20 -0500

    various bug fixes

--- src/services/OrderDelivererService.jsx
+++ src/services/OrderDelivererService.jsx
@@ -1,4 +1,3 @@
 export const getAllOrderDeliverers = () => {
-    return fetch(`http://localhost:8088/orderDeliverers?_expand=user`)
-        .then (res => res.json())
+    return fetch(`http://localhost:8088/orderDeliverers?_expand=user`).then(res => res.json())
 }
