67a621ac8c56bf24d9ab95aebcad332fbd1bbf1b
Andrew <goodman.drew1@gmail.com>
2024-04-11 14:41:29 -0500

    undefined error

--- src/services/PizzaServices.jsx
+++ src/services/PizzaServices.jsx
@@ -1,57 +1,72 @@
 export const getPizzasByOrderId = async (orderId) => {
-    return fetch(`http://localhost:8088/pizzas?orderId=${orderId}&_expand=size&_expand=cheese&_expand=sauce`).then((res) => res.json())
-  }
+  return fetch(
+    `http://localhost:8088/pizzas?orderId=${orderId}&_expand=size&_expand=cheese&_expand=sauce`
+  ).then((res) => res.json());
+};
 
-
-  export const getToppingsByPizzaId = async (pizzaId) => {
-    return fetch(`http://localhost:8088/pizzaToppings?pizzaId=${pizzaId}&_expand=topping`).then((res) => res.json())
-}
-
+export const getToppingsByPizzaId = async (pizzaId) => {
+  return fetch(
+    `http://localhost:8088/pizzaToppings?pizzaId=${pizzaId}&_expand=topping`
+  ).then((res) => res.json());
+};
 
 export const getAllPizzaToppings = async (/* pizzaId */) => {
-    return fetch(`http://localhost:8088/pizzaToppings?_expand=topping`).then((res) => res.json())
-}
+  return fetch(`http://localhost:8088/pizzaToppings?_expand=topping`).then(
+    (res) => res.json()
+  );
+};
 
 export const deletePizzaById = async (pizzaId) => {
-    // Fetch all pizza toppings
-    const toppingsResponse = await fetch(`http://localhost:8088/pizzaToppings`)
-    const allPizzaToppings = await toppingsResponse.json()
-  
-    // filtertoppings by pizzID
-    const pizzaToppingsToDelete = allPizzaToppings.filter(topping => topping.pizzaId === pizzaId)
-  debugger
-    // delete each topping obj 1 by 1
-    await pizzaToppingsToDelete.map(async (topping) => {
-      await fetch(`http://localhost:8088/pizzaToppings/${topping.id}`, {
-        method: 'DELETE',
-      })
-    })
-  
-    // Delete the pizza obj
-    await fetch(`http://localhost:8088/pizzas/${pizzaId}`, {
+  // Fetch all pizza toppings
+  const toppingsResponse = await fetch(`http://localhost:8088/pizzaToppings`);
+  const allPizzaToppings = await toppingsResponse.json();
+
+  // filtertoppings by pizzID
+  const pizzaToppingsToDelete = allPizzaToppings.filter(
+    (topping) => topping.pizzaId === pizzaId
+  );
+  debugger;
+  // delete each topping obj 1 by 1
+  await pizzaToppingsToDelete.map(async (topping) => {
+    await fetch(`http://localhost:8088/pizzaToppings/${topping.id}`, {
       method: "DELETE",
-    })
-  
-    return true
-  }
-  
-  
-  export const getAllPizzaToppingObjects = async () => {
-    return fetch(`http://localhost:8088/pizzaToppings`).then((res) => res.json())
-  }
+    });
+  });
 
-  export const getAllSizes = async () => {
-    return fetch(`http://localhost:8088/sizes`).then((res) => res.json())
-  }
+  // Delete the pizza obj
+  await fetch(`http://localhost:8088/pizzas/${pizzaId}`, {
+    method: "DELETE",
+  });
 
-  export const getAllCheeses = async () => {
-    return fetch(`http://localhost:8088/cheeses`).then((res) => res.json())
-  }
+  return true;
+};
 
-  export const getAllSauces= async () => {
-    return fetch(`http://localhost:8088/sauces`).then((res) => res.json())
-  }
+export const getAllPizzaToppingObjects = async () => {
+  return fetch(`http://localhost:8088/pizzaToppings`).then((res) => res.json());
+};
 
-  export const getAllToppings = async () => {
-    return fetch(`http://localhost:8088/toppings`).then((res) => res.json())
-  }
+export const getAllSizes = async () => {
+  return fetch(`http://localhost:8088/sizes`).then((res) => res.json());
+};
+
+export const getAllCheeses = async () => {
+  return fetch(`http://localhost:8088/cheeses`).then((res) => res.json());
+};
+
+export const getAllSauces = async () => {
+  return fetch(`http://localhost:8088/sauces`).then((res) => res.json());
+};
+
+export const getAllToppings = async () => {
+  return fetch(`http://localhost:8088/toppings`).then((res) => res.json());
+};
+
+export const createNewPizza = async (pizzaObj) => {
+  await fetch(`http://localhost:8088/pizzas`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(pizzaObj),
+  });
+};
--- src/views/AdminViews.jsx
+++ src/views/AdminViews.jsx
@@ -1,37 +1,61 @@
-import { Outlet, Route, Routes } from "react-router-dom"
-import { OrderDetails } from "../components/orders/OrderDetails" // Import OrderDetails component
+import { Outlet, Route, Routes } from "react-router-dom";
+import { OrderDetails } from "../components/orders/OrderDetails"; // Import OrderDetails component
 
-import { useEffect, useState } from "react"
-import NavBar from "../components/nav/NavBar"
-import { OrderList } from "../components/orders/OrderList"
-import { NewOrder } from "../components/orders/NewOrder"
-import { getAllUsers } from "../services/userService"
-import { SalesReport } from "../sales/SalesReport"
+import { useEffect, useState } from "react";
+import NavBar from "../components/nav/NavBar";
+import { OrderList } from "../components/orders/OrderList";
+import { NewOrder } from "../components/orders/NewOrder";
+import { getAllUsers } from "../services/userService";
+import { SalesReport } from "../sales/SalesReport";
+import { Pizzas } from "../components/pizzas/Pizzas.jsx";
 
+export const AdminViews = ({
+  currentUser,
+  service,
+  setService,
+  employees,
+  setEmployees,
+}) => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <NavBar currentUser={currentUser} />
+            <Outlet />
+          </>
+        }
+      >
+        <Route index element={<h1>yuh</h1>} />
 
-export const AdminViews = ({currentUser, service, setService, employees, setEmployees}) => {
-
-
-
-    return (
-        <Routes>
-            <Route path="/" element={
-                <>
-                    <NavBar currentUser={currentUser} />
-                    <Outlet />
-                </>
-            }>
-                <Route index element={<h1>yuh</h1>} />
-             
-                <Route path='/orderList' >
-                    <Route index element={<OrderList currentUser={currentUser} />}/>
-                    <Route path=':orderId' element={<OrderDetails currentUser={currentUser} service={service} employees={employees}/>} />
-                    
-                </Route>
-                <Route path='/NewOrder' element={<NewOrder service={service} setService={setService} currentUser={currentUser}/>} />
-                <Route path='/Employees' element={<h1>employye list here bruh</h1>} />
-                <Route path='/SalesReport' element={<SalesReport />} />
-            </Route>
-        </Routes>
-    )
-}
+        <Route path="/orderList">
+          <Route index element={<OrderList currentUser={currentUser} />} />
+          <Route
+            path=":orderId"
+            element={
+              <OrderDetails
+                currentUser={currentUser}
+                service={service}
+                employees={employees}
+              />
+            }
+          />
+          <Route path=":orderId/:pizzaId" element={<Pizzas />} />
+        </Route>
+        <Route
+          path="/NewOrder"
+          element={
+            <NewOrder
+              service={service}
+              setService={setService}
+              currentUser={currentUser}
+            />
+          }
+        />
+        <Route path="/Employees" element={<h1>employye list here bruh</h1>} />
+        <Route path="/SalesReport" element={<SalesReport />} />
+      </Route>
+    </Routes>
+  );
+};
